f75d3ffaf6845afbbc794cde7e18187fb6682f47
libertus-codelin <codelinkursus@gmail.com>
2023-01-25 20:38:13 +0700

    javascript

--- scripts/td-string.js
+++ scripts/td-string.js
@@ -1,3 +1,16 @@
 document.writeln("Libertus Sabebeget");
 document.writeln("<br>");
 document.writeln("Belajar Javascript dengan Programmer Zaman Now");
+document.writeln("<br>");
+
+document.writeln("Nama " + "Libertus");
+document.writeln("<br>");
+document.writeln("Chanel : " + "Libertus Devlin");
+document.writeln("<br>");
+
+document.writeln("<textarea cols='100' rows='10'>");
+document.write("Libertus \nSabebeget\n");
+document.write("\\Home \\Libertus\n");
+document.write("\"Libertus Devlin\"\n");
+document.write("\'Belajar Javascript dengan Programmer Zaman\'\n");
+document.writeln("</textarea>");
